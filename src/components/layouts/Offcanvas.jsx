0d80d86628cdd702d8580b9790e24c8ef1030e57
ashminashref <ashminashraf07@gmail.com>
2025-12-16 11:10:19 +0530

    updated sidebar

--- src/components/layouts/Offcanvas.jsx
+++ src/components/layouts/Offcanvas.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // MUI
 import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
 import GroupIcon from '@mui/icons-material/Group';
@@ -9,17 +9,52 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import CampaignIcon from '@mui/icons-material/Campaign';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import './Offcanvas.css'
+
 function Offcanvas() {
   return (
-    <div className='mt-5 pt-5   d-flex flex-column gap-4 align-items-start p-3 sidebar'>
-                <button className='side-btn d-flex align-items-center gap-2'> <SpaceDashboardIcon/>Dashboard</button>
-        <button className='side-btn d-flex align-items-center gap-2'> <GroupIcon/>Users</button>
-        <button className='side-btn d-flex align-items-center gap-2'><FamilyRestroomIcon/> Families </button>
-        <button className='side-btn d-flex align-items-center gap-2'><CurrencyRupeeIcon/> Payments </button>
-        <button className='side-btn d-flex align-items-center gap-2'><CampaignIcon/> Announcements </button>
-        <button className='side-btn d-flex align-items-center gap-2'><DescriptionIcon/>Certificates </button>
-        <button className='side-btn d-flex align-items-center gap-2'><NotificationsNoneIcon/>Notifications</button>
-     
+    <div className='mt-5 pt-5 d-flex flex-column justify-content-between align-items-start p-3 sidebar'>
+        
+        {/* Dashboard */}
+        <button className='side-btn d-flex align-items-center gap-2'> 
+            <SpaceDashboardIcon/> 
+            <span className="d-none d-md-block">Dashboard</span>
+        </button>
+
+        {/* Users */}
+        <button className='side-btn d-flex align-items-center gap-2'> 
+            <GroupIcon/> 
+            <span className="d-none d-md-block">Users</span>
+        </button>
+
+        {/* Families */}
+        <button className='side-btn d-flex align-items-center gap-2'>
+            <FamilyRestroomIcon/> 
+            <span className="d-none d-md-block">Families</span> 
+        </button>
+
+        {/* Payments */}
+        <button className='side-btn d-flex align-items-center gap-2'>
+            <CurrencyRupeeIcon/> 
+            <span className="d-none d-md-block">Payments</span> 
+        </button>
+
+        {/* Announcements */}
+        <button className='side-btn d-flex align-items-center gap-2'>
+            <CampaignIcon/> 
+            <span className="d-none d-md-block">Announcements</span> 
+        </button>
+
+        {/* Certificates */}
+        <button className='side-btn d-flex align-items-center gap-2'>
+            <DescriptionIcon/>
+            <span className="d-none d-md-block">Certificates</span> 
+        </button>
+
+        {/* Notifications */}
+        <button className='side-btn d-flex align-items-center gap-2'>
+            <NotificationsNoneIcon/>
+            <span className="d-none d-md-block">Notifications</span>
+        </button>
 
     </div>
   )
